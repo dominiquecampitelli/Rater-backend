6b2c91810dfb0ffb22fed73fee956b3cacc59294
Dominique Campitelli <dominiquecamp@outlook.com>
2024-08-16 22:48:15 -0300

    Port

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,14 @@ app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ message: error.message });
 });
 
+const port = process.env.PORT || 3333;
+
 const start = async () => {
   await app.register(cors);
   await app.register(routes);
 
   try {
-    await app.listen({ port: 3333 });
+    await app.listen({ port: Number(port) });
   } catch (err) {
     process.exit(1);
   }
